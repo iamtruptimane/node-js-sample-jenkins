6b25476a94402e1233d75410fbd439fa15df06e0
iamtruptimane <iamtruptimane@gmail.com>
2025-04-25 18:02:15 +0530

    modified index.js file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Succesfully deployed my first application through CICD!!!')
+  response.send('wohooo Succesfully deployed my first application through CICD!!!')
 })
 
 app.listen(app.get('port'), function() {
