081763b22be301d6b1baba9e10acd90ce0e5876a
iamtruptimane <iamtruptimane@gmail.com>
2025-04-25 18:19:20 +0530

    added images

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('wohooo Succesfully deployed my first application through CICD!!!')
-})
+// Serve static files from public directory
+app.use(express.static(__dirname + '/public'));
+
+// Serve index.html file on root request
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
